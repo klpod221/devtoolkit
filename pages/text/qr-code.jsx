085d429c86d9f306f4be249da7ed160d7bebd3be
klpod221 <klpod221@gmail.com>
2024-10-03 16:48:35 +0700

    Refactor QR Code Generator to use range sliders for width and margin, update error correction level, and enhance output display

--- pages/text/qr-code.jsx
+++ pages/text/qr-code.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import QRCode from "qrcode";
 
-import debounce from "@utils/debounce";
-
 import TwoColumn from "@components/TwoColumn";
 import MyCard from "@components/MyCard";
 import MyCodeEditor from "@components/MyCodeEditor";
 import MyImage from "@components/MyImage";
-import MyInput from "@components/MyInput";
 import MySelect from "@components/MySelect";
 import MyColorPicker from "@components/MyColorPicker";
+import MyRangeSlider from "@components/MyRangeSlider";
 
 const errorCorrectionLevels = ["L", "M", "Q", "H"];
 
@@ -19,7 +17,7 @@ const QRCodeGenerator = () => {
   const [options, setOptions] = React.useState({
     width: 500,
     margin: 1,
-    errorCorrectionLevel: "H",
+    errorCorrectionLevel: "M",
     type: "image/png",
     quality: 1,
     color: {
@@ -52,78 +50,70 @@ const QRCodeGenerator = () => {
         <MyCodeEditor language="text" value={input} onChange={setInput} />
       </TwoColumn.Left>
       <TwoColumn.Right>
-        <MyCard.Header title="Output" helper="QR Code" />
+        <MyCard.Header title="Output" helper="QR Code with custom options" />
 
-        <div className="flex items-center flex-wrap gap-4">
-          <MyInput
-            label="Width"
-            type="number"
-            value={options.width}
-            onChange={(value) => setOptions({ ...options, width: value })}
-            max={1000}
-            min={1}
-          />
+        <div className="overflow-y-auto">
+          <div className="flex flex-col gap-2 w-full">
+            <MyRangeSlider
+              label="Width"
+              value={options.width}
+              onChange={(value) => setOptions({ ...options, width: value })}
+              max={1000}
+              min={150}
+            />
 
-          <MyInput
-            label="Margin"
-            type="number"
-            value={options.margin}
-            onChange={(value) => setOptions({ ...options, margin: value })}
-            max={100}
-            min={0}
-          />
+            <MyRangeSlider
+              label="Margin"
+              value={options.margin}
+              onChange={(value) => setOptions({ ...options, margin: value })}
+              max={100}
+              min={0}
+            />
 
-          <MySelect
-            label="Correction Level"
-            value={options.errorCorrectionLevel}
-            sizing="md"
-            onChange={(value) =>
-              setOptions({ ...options, errorCorrectionLevel: value })
-            }
-          >
-            {errorCorrectionLevels.map((level) => (
-              <option key={level} value={level}>
-                {level}
-              </option>
-            ))}
-          </MySelect>
+            <MySelect
+              label="Correction Level"
+              value={options.errorCorrectionLevel}
+              sizing="md"
+              onChange={(value) =>
+                setOptions({ ...options, errorCorrectionLevel: value })
+              }
+            >
+              {errorCorrectionLevels.map((level) => (
+                <option key={level} value={level}>
+                  {level}
+                </option>
+              ))}
+            </MySelect>
+          </div>
 
-          <MyInput
-            label="Quality"
-            type="number"
-            value={options.quality}
-            onChange={(value) => setOptions({ ...options, quality: value })}
-            max={1}
-            min={0}
-            step={0.1}
-          />
+          <div className="flex flex-wrap items-center gap-4">
+            <MyColorPicker
+              label="QR Color"
+              value={options.color.dark}
+              onChange={(value) =>
+                setOptions({
+                  ...options,
+                  color: { ...options.color, dark: value },
+                })
+              }
+            />
+
+            <MyColorPicker
+              label="Background Color"
+              value={options.color.light}
+              onChange={(value) =>
+                setOptions({
+                  ...options,
+                  color: { ...options.color, light: value },
+                })
+              }
+            />
+          </div>
+
+          <div className="flex flex-col gap-2 w-full mt-2">
+            {qrCode && <MyImage src={qrCode} alt="QR Code" />}
+          </div>
         </div>
-
-        <div className="flex flex-wrap items-center gap-4">
-          <MyColorPicker
-            label="QR Color"
-            value={options.color.dark}
-            onChange={(value) =>
-              setOptions({
-                ...options,
-                color: { ...options.color, dark: value },
-              })
-            }
-          />
-
-          <MyColorPicker
-            label="Background Color"
-            value={options.color.light}
-            onChange={(value) =>
-              setOptions({
-                ...options,
-                color: { ...options.color, light: value },
-              })
-            }
-          />
-        </div>
-
-        {qrCode && <MyImage src={qrCode} alt="QR Code" />}
       </TwoColumn.Right>
     </TwoColumn>
   );
