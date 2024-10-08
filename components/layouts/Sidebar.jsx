17e20b9ef65f691301a3cb63939d2086e77eb321
klpod221 <klpod221@gmail.com>
2024-10-08 17:14:08 +0700

    Remove unnecessary classes from "No tools found!" message in Sidebar component

--- components/layouts/Sidebar.jsx
+++ components/layouts/Sidebar.jsx
@@ -84,7 +84,7 @@ const MySidebar = ({ isOpen, setIsOpen }) => {
 
           {/* show error message if no tools found */}
           {toolkit.length === 0 && (
-            <div className="text-center text-gray-500 dark:text-gray-400 mt-4">
+            <div className="text-center mt-4">
               No tools found!
             </div>
           )}
