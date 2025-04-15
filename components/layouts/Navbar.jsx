9e69fe6c69b33b0550053928ee5c8906cee87e2b
klpod221 <klpod221@gmail.com>
2025-04-15 13:56:04 +0700

    Update navbar title from "DevToolkit" to "DevTools" for consistency with branding

--- components/layouts/Navbar.jsx
+++ components/layouts/Navbar.jsx
@@ -7,7 +7,7 @@ import { HiBars3BottomLeft } from "react-icons/hi2";
 
 import { ThemeContext } from "@/providers/ThemeProvider";
 
-const MyNavbar = ({ isOpen, setIsOpen, title = "DevToolkit" }) => {
+const MyNavbar = ({ isOpen, setIsOpen, title = "DevTools" }) => {
   const { theme, setTheme } = React.useContext(ThemeContext);
 
   const toggleSidebar = () => {
@@ -44,7 +44,7 @@ const MyNavbar = ({ isOpen, setIsOpen, title = "DevToolkit" }) => {
                   height: "auto"
                 }} />
               <span className="self-center text-lg font-semibold sm:text-xl whitespace-nowrap ">
-                DevToolkit
+                DevTools
               </span>
             </NextLink>
           </div>
